c28cf8e346f02287373227a01b7159cbaf323222
Jules Sanchez <jules.n.sanchez@gmail.com>
2023-04-18 16:38:06 +0200

    dev files removed

--- debug.py
+++
@@ -1,8 +0,0 @@
-import importlib
-import argparse
-from omegaconf import OmegaConf
-import os.path as osp
-
-target_data_cfg = OmegaConf.load(osp.join('cfg/data_cfg',"semantic-kitti.yaml"))
-
-print(target_data_cfg.learning_map)
